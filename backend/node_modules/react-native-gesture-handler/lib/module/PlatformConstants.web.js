ee96efd17bd3112599547f1445ce0dbe15587824
Yogarajalakshmi S <yogarajalakshmis@gmail.com>
2025-02-08 15:20:15 -0500

    Initial commit

---
+++ backend/node_modules/react-native-gesture-handler/lib/module/PlatformConstants.web.js
@@ -0,0 +1,6 @@
+export default {
+  get forceTouchAvailable() {
+    return false;
+  }
+};
+//# sourceMappingURL=PlatformConstants.web.js.map
---
+++ backend/node_modules/react-native-gesture-handler/lib/module/web_hammer/Errors.js
@@ -0,0 +1,6 @@
+export class GesturePropError extends Error {
+  constructor(name, value, expectedType) {
+    super(`Invalid property \`${name}: ${value}\` expected \`${expectedType}\``);
+  }
+}
+//# sourceMappingURL=Errors.js.map
